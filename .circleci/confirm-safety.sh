3f51c0261000146a8fc9ed15a178a65b10f3ccd1
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-07-19 16:45:32 -0700

    Try harder to get an accurate api URL

--- .circleci/confirm-safety.sh
+++ .circleci/confirm-safety.sh
@@ -5,6 +5,15 @@ if [ -z "$CIRCLECI" ] ; then
   exit 0
 fi
 
+if [ -z "$CIRCLE_PULL_REQUEST" ] ; then
+  echo "No CIRCLE_PULL_REQUEST defined"
+  exit 1
+fi
+
+// CIRCLE_PULL_REQUEST=https://github.com/greg-1-anderson/WordPress/pull/6
+PR_NUMBER=$(echo CIRCLE_PULL_REQUEST | sed -e 's#.*/pull/##')
+
+echo curl https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/$CIRCLE_PR_NUMBER
 curl https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/$CIRCLE_PR_NUMBER
 
 base=$(curl https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/$CIRCLE_PR_NUMBER 2>/dev/null | jq .base.ref)
